7c648fb6cacec267178e92a66b126cc58e9dc843
brownquartz <parkcm2262@gmail.com>
2025-06-24 17:33:33 +0900

    delete <Router> tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 // import { AuthContext, AuthProvider } from '../contexts/AuthContext'
 import Layout     from './components/layout/Layout';
 import PostsList  from './pages/PostsList';
@@ -10,20 +10,18 @@ import Login      from './pages/Login';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {/* Layout を使うルート */}
-        <Route path="/" element={<Layout />}>
-          <Route index        element={<Navigate to="" replace />} />
-          <Route path="posts">
-           <Route index element={<PostsList />} />
-           <Route path="new" element={<PostForm />} />
-           <Route path=":id" element={<PostDetail />} />
-           <Route path=":id/edit" element={<EditPost />} />
-         </Route>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      {/* Layout を使うルート */}
+      <Route path="/" element={<Layout />}>
+        <Route index        element={<Navigate to="" replace />} />
+        <Route path="posts">
+          <Route index element={<PostsList />} />
+          <Route path="new" element={<PostForm />} />
+          <Route path=":id" element={<PostDetail />} />
+          <Route path=":id/edit" element={<EditPost />} />
         </Route>
-      </Routes>
-    </Router>
+      </Route>
+    </Routes>
   );
 }
